09b4f4cddaaac335cfffd835fd460c98d7104c9d
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2024-07-01 19:08:04 +0545

    add Lesson 3

---
+++ LESSON03/meaning.py
@@ -0,0 +1,12 @@
+# Operators
+meaning = 52
+print('')
+
+# if meaning > 10:
+#     print('Carry on')
+# else:
+#     print('Not Today')
+
+
+# Ternary Operator
+print('Carry on!') if meaning > 10 else print('Not Today!')
